0d84f5c03f43a4a7dd074e5f260f8f8c1bd357ce
ztarikul <ztarikulislam@gmail.com>
2022-08-12 10:46:02 +0600

    auth

--- passport/app/Http/Controllers/AuthController.php
+++ passport/app/Http/Controllers/AuthController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AuthController extends Controller
 {
     //
+    public function login(Request $request)
+    {
+
+        try {
+            if (Auth::attempt($request->only('email', 'password'))) {
+                $user = Auth::user();
+                $token = $user->createToken('app')->accessToken;
+
+                return response([
+                    'message' => "Successfully Login",
+                    'token' => $token,
+                    'user' => $user,
+                ], 200);
+            }
+        } catch (Exception $exception) {
+            return response([
+                'message' => $exception->getMessage()
+            ], 400);
+
+            return response([
+                'messsage' => 'Invalid Email Or Password'
+            ], 401);
+        }
+    } //end method
+
+
+    public function register(RegisterRequest $request)
+    {
+    }
 }
